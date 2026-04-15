dd010e0202dca23a8f8978976c4a66a6edb5c365
Jeet Upadhyay <jeetupadhyay1204@gmail.com>
2026-04-15 20:32:28 +0200

    Dynamic import of modules based on path

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
-console.log('AURA initialized')
+const path = window.location.pathname;
+if (path.includes('app-screens')) {
+  import('./modules/ui.js');
+} else {
+  import('./modules/landing.js');
+}
